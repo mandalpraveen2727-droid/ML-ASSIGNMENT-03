023734a48f75bdd89090f5c83290f03f6c280d15
mandalpraveen2727-droid <mandalpraveen2727@gmail.com>
2026-09-22 23:52:43 +0530

    Update TASK2.py

--- TASK2.py
+++ TASK2.py
@@ -5,7 +5,6 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
-# Create the dataset
 data = {
     "Age": [22, 25, None, 30, 35, 40, None, 28],
     "Salary": [25000, 32000, 40000, None,55000, 65000, 45000, None],
@@ -16,31 +15,24 @@ df = pd.DataFrame(data)
 print("Original Dataset:")
 print(df)
 
-# Define the columns
 numerical_features = ["Age","Salary","Years_of_Experience"]
 categorical_features = ["Department"]
 
-# Numerical preprocessing using MinMaxScaler
 numerical_pipeline = Pipeline(
     steps=[("imputer", SimpleImputer(strategy="median")),("scaler", MinMaxScaler())])
 
-# Categorical preprocessing
 categorical_pipeline = Pipeline(
     steps=[("imputer",SimpleImputer(strategy="most_frequent")),
         ("encoder",OneHotEncoder(handle_unknown="ignore",sparse_output=False))])
 
-# Combine the pipelines
 preprocessor = ColumnTransformer(
     transformers=[("numerical",numerical_pipeline,numerical_features),
         ("categorical",categorical_pipeline,categorical_features)])
 
-# Apply the preprocessing
 processed_data = preprocessor.fit_transform(df)
 
-# Get transformed feature names
 column_names = preprocessor.get_feature_names_out()
 
-# Convert into a DataFrame
 processed_df = pd.DataFrame(
     processed_data,
     columns=column_names
