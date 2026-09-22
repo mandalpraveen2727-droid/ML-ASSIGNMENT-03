c00596a60072c193dbc97088b9aa8246d21d4a7d
mandalpraveen2727-droid <mandalpraveen2727@gmail.com>
2026-09-22 23:55:34 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,39 +6,29 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 
-# 1. Create Mock Dataset
 data = {"Age": [20, 21, None, 23, 24],"Income": [25000, 30000, 28000, None, 40000],
     "City": ["Kolkata", "Durgapur", "Kolkata", "Asansol", "Durgapur"],"Purchased": [0, 1, 1, 0, 1]}
 
 df = pd.DataFrame(data)
-# Separate Features and Target
 X = df.drop("Purchased", axis=1)
 y = df["Purchased"]
 numerical_features = ["Age", "Income"]
 categorical_features = ["City"]
 
-# 2. Define Preprocessing Pipelines
-
-# Pipeline for numerical features
 numerical_transformer = Pipeline(steps=[("imputer", SimpleImputer(strategy="median")),("scaler", StandardScaler())])
 
-# Pipeline for categorical features
 categorical_transformer = Pipeline(
     steps=[("imputer", SimpleImputer(strategy="most_frequent")),
         ("onehot",OneHotEncoder(handle_unknown="ignore",sparse_output=False))])
 
-# 3. Combine Using ColumnTransformer
 preprocessor = ColumnTransformer(
     transformers=[("num",numerical_transformer,numerical_features),
         ("cat",categorical_transformer,categorical_features)])
 
-# Apply Transformations
 X_processed = preprocessor.fit_transform(X)
 
-# 4. Train-Test Split
 X_train, X_test, y_train, y_test = train_test_split(X_processed, y, test_size=0.2, random_state=42)
 
-# 5. Display Results
 print("---- Original Dataset ----")
 print(df)
 
