197c3f4dbcc8314840461701e64f6484e8df23fd
mandalpraveen2727-droid <mandalpraveen2727@gmail.com>
2026-09-22 23:53:42 +0530

    Update TASK3.py

--- TASK3.py
+++ TASK3.py
@@ -3,7 +3,6 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-# Create the numerical dataset
 data = {
     "Age": [22, 25, None, 30, 35, 40, None, 28],
     "Salary": [25000, 32000, 40000, None,55000, 65000, 45000, None],
@@ -14,21 +13,16 @@ df = pd.DataFrame(data)
 print("Original Numerical Dataset:")
 print(df)
 
-# Pipeline using StandardScaler
 standard_pipeline = Pipeline(steps=[("imputer", SimpleImputer(strategy="median")),("scaler", StandardScaler())])
 
-# Pipeline using MinMaxScaler
 minmax_pipeline = Pipeline(steps=[("imputer", SimpleImputer(strategy="median")),("scaler", MinMaxScaler())])
 
-# Apply both transformations
 standard_scaled_data = standard_pipeline.fit_transform(df)
 minmax_scaled_data = minmax_pipeline.fit_transform(df)
 
-# Convert transformed arrays into DataFrames
 standard_df = pd.DataFrame(standard_scaled_data,columns=df.columns)
 minmax_df = pd.DataFrame(minmax_scaled_data,columns=df.columns)
 
-# Display StandardScaler result
 print("\nData After StandardScaler:")
 print(standard_df)
 
@@ -41,7 +35,6 @@ print(standard_df.max())
 print("\nStandardScaler Mean Values:")
 print(standard_df.mean())
 
-# Display MinMaxScaler result
 print("\nData After MinMaxScaler:")
 print(minmax_df)
 
